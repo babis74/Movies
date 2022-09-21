957126806cfe2595aaadb256a12b664773a401da
Babis <Babis@DESKTOP-0FDE96H>
2022-09-21 15:56:10 +0300

    Add project files.

---
+++ Movies/Data/MoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace Movies.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
